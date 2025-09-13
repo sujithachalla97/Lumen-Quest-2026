354be27862d62459724360f65f0b08f0d1236e3c
Gauri Agrawal <agrawalgauri866@gmail.com>
2025-09-13 13:44:55 +0530

    Add files via upload

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 // src/routes/auth.js
 import express from 'express';
-import { login, register, me } from '../controllers/authController.js';
+import { login, register,me } from '../controllers/authController.js';
 import { requireAuth } from '../middleware/auth.js';
+
 
 const router = express.Router();
 
